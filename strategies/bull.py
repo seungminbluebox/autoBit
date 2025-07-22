a31cfb3b26b4856fe59851743c985030ff13a15b
Seung Min <parksm1415@gmail.com>
2025-07-22 18:30:09 +0900

    bull 매도판단 조정

--- strategies/bull.py
+++ strategies/bull.py
@@ -15,8 +15,8 @@ def should_sell(data, btc_balance, avg_price):
     rsi = data["rsi"]
     prev_rsi = data["prev_rsi"]
 
-    if profit_ratio >= 0.05:
-        return True, 1.0
+    # if profit_ratio >= 0.05:
+    #     return True, 1.0
     if profit_ratio >= 0.03 and rsi < prev_rsi:
         return True, 1.0
 
